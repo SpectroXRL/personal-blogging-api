a4c938146ff0232e10b592356aa0b213176562ac
SpectroXRL <spectrothedev@gmail.com>
2025-05-15 01:23:17 -0500

    Created Article Model

---
+++ PersonalBloggingApi/Models/Article.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PersonalBloggingApi.Models
+{
+    public class Article
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string? Title { get; set; }
+
+        [Required]
+        public string? Body { get; set; }
+
+        [Required]
+        public string? Author { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+    }
+}
