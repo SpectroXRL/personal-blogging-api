71c6a42bcbcdff1f488541d03ed7adc3bf04d86d
SpectroXRL <spectrothedev@gmail.com>
2025-05-19 15:19:43 -0500

    Refactored all CRUD Operations to return their respective status codes

--- PersonalBloggingApi/Program.cs
+++ PersonalBloggingApi/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using PersonalBloggingApi.Models;
 
@@ -6,17 +7,31 @@ var app = builder.Build();
 
 List<Article> articles = new List<Article>{};
 
-app.MapGet("/articles", () => articles);
+app.MapGet("/articles", () => TypedResults.Ok(articles));
 
-app.MapGet("/articles/{id}", (int id) => articles.Find(article => article.Id == id));
+app.MapGet("/articles/{id}", Results<Ok<Article>, NotFound> (int id) => 
+{
+    var article = articles.Find(article => article.Id == id);
+
+    if(article != null)
+    {
+        return TypedResults.Ok(article);
+    }
+
+    return TypedResults.NotFound();
+
+
+})
+    .WithName("GetArticleById");
 
 app.MapPost("/articles", ([FromBody]Article article) => {
         article.Id = articles.Count != 0 ? articles.Max(article => article.Id) + 1 : 0;
         article.CreatedAt = article.LastEdited = DateOnly.FromDateTime(DateTime.Now);
         articles.Add(article);
+        return TypedResults.CreatedAtRoute(article, "GetArticleById", new { id = article.Id});
     });
 
-app.MapPut("/articles/{id}", (int id, [FromBody]Article updatedArticle) =>
+app.MapPut("/articles/{id}", Results<NoContent, NotFound> (int id, [FromBody]Article updatedArticle) =>
 {
     Article? existingArticle = articles.Find(article => article.Id == id);
     if (existingArticle != null)
@@ -25,20 +40,23 @@ app.MapPut("/articles/{id}", (int id, [FromBody]Article updatedArticle) =>
         existingArticle.Body = updatedArticle.Body;
         existingArticle.Author = updatedArticle.Author;
         existingArticle.LastEdited = DateOnly.FromDateTime(DateTime.Now);
+        return TypedResults.NoContent();
     }
     else
     {
-
+        return TypedResults.NotFound();
     }
 });
 
-app.MapDelete("/articles/{id}", (int id) =>
+app.MapDelete("/articles/{id}", Results<NoContent, NotFound> (int id) =>
 {
     Article? article = articles.Find(article => article.Id == id);
     if(article != null)
     {
         articles.Remove(article);
+        return TypedResults.NoContent();
     }
+    return TypedResults.NotFound();
 });
 
 app.Run();
