8b26ce74824b7ee53202d206842e8b2bf5d67378
SpectroXRL <spectrothedev@gmail.com>
2025-05-21 02:34:31 -0500

    Setup Articles Db Context and Migrations

---
+++ PersonalBloggingApi/Data/ArticlesDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PersonalBloggingApi.Models;
+
+namespace PersonalBloggingApi.Data
+{
+    public class ArticlesDbContext : DbContext
+    {
+        public ArticlesDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Article> Articles { get; set; }
+    }
+}
